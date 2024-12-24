31cc516e0ce6a3ced34e11f03dcd2c977e05ee1a
mixednuts92 <reedjaisen31@gmail.com>
2024-12-23 21:53:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["I love you roddd <3333"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
